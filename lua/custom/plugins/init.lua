4eca8c45af06140fdfbf4d09c09ee80a8a5bb921
Ale <7repip@gmail.com>
2025-05-07 22:35:53 +0200

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,6 +6,16 @@
 vim.opt.conceallevel = 1
 return {
   {
+    'goolord/alpha-nvim',
+    dependencies = {
+      'echasnovski/mini.icons',
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('alpha').setup(require('alpha.themes.startify').config)
+    end,
+  },
+  {
     'NeogitOrg/neogit',
     dependencies = {
       'nvim-lua/plenary.nvim', -- required
@@ -59,30 +69,64 @@ return {
     },
   },
   {
-    'epwalsh/obsidian.nvim',
+    'obsidian-nvim/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = false,
     ft = 'markdown',
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
+    event = {
+      --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+      --   -- refer to `:h file-pattern` for more examples
+      'BufReadPre /home/ale/Nextcloud2/Obsidian/Matrix/*.md',
+      'BufNewFile /home/ale/Nextcloud2/Obsidian/Matrix/*.md', -- path/to/my-vault/*.md",
+    },
     dependencies = {
       -- Required.
       'nvim-lua/plenary.nvim',
 
       -- see below for full list of optional dependencies 👇
     },
+    ---@module 'obsidian'
+    ---@type obsidian.config.ClientOpts
     opts = {
       workspaces = {
         {
           name = 'Matrix',
           path = '~/Nextcloud2/Obsidian/Matrix',
         },
+      },
+
+      -- Alternatively - and for backwards compatibility - you can set 'dir' to a single path instead of
+      -- 'workspaces'. For example:
+      -- dir = "~/vaults/work",
+
+      -- Optional, if you keep notes in a specific subdirectory of your vault.
+      -- notes_subdir = 'INBOX',
+
+      -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log
+      -- levels defined by "vim.log.levels.*".
+      log_level = vim.log.levels.INFO,
+
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = 'INBOX',
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = '%Y-%m-%d',
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = '%B %-d, %Y',
+        -- Optional, default tags to add to each new daily note created.
+        default_tags = { 'daily-notes' },
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = nil,
+      },
+
+      -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
       },
 
       -- see below for full list of options 👇
